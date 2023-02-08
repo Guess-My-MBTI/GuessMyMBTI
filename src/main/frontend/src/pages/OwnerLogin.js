c3c0b5abd12e412103b7a8651199f4a3953f86a9
SU_VIN <pyotnqls25@naver.com>
2023-02-08 17:56:45 +0900

    OwnerLogin_banner setting

--- src/main/frontend/src/pages/OwnerLogin.js
+++ src/main/frontend/src/pages/OwnerLogin.js
@@ -1,4 +1,31 @@
 const OwnerLogin = () => {
-  return <div className="OwnerLogin">로그인</div>;
+  return (
+    <div className="OwnerLogin">
+      <div className="banner">
+        <p className="me">
+          <img
+            src={process.env.PUBLIC_URL + `assets/pencil1.png`}
+            className="pencil1"
+          />
+          나의
+        </p>
+
+        <p className="mbti">
+          <img
+            src={process.env.PUBLIC_URL + `assets/pencil2.png`}
+            className="pencil2"
+          />
+          <p className="m">M</p>
+          <p className="b">B</p>
+          <p className="t">T</p>
+          <p className="i">I</p>
+          <p className="r">를</p>
+        </p>
+
+        <p className="guess">맞춰봐</p>
+      </div>
+      <div className="login_wrapper"></div>
+    </div>
+  );
 };
 export default OwnerLogin;
