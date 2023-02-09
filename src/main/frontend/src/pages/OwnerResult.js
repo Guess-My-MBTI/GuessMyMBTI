4087cdb9d5b2cfce80417b7ec6c9cced485b6246
SU_VIN <pyotnqls25@naver.com>
2023-02-09 16:39:55 +0900

    OwnerResult 깡통완성
    반응형적용

--- src/main/frontend/src/pages/OwnerResult.js
+++ src/main/frontend/src/pages/OwnerResult.js
@@ -1,4 +1,45 @@
+import { AiFillHome } from "react-icons/ai";
+import { HiOutlineLink } from "react-icons/hi";
+import { MdOutlineReplay } from "react-icons/md";
 const OwnerResult = () => {
-  return <div className="OwnerResult"></div>;
+  return (
+    <div className="OwnerResult">
+      <div className="result">
+        <p className="r">RESULT</p>
+        <div className="mbti-box">
+          <div className="mbti">
+            <p className="m">M</p>
+            <p className="b">B</p>
+            <p className="t">T</p>
+            <p className="i">I</p>
+          </div>
+        </div>
+      </div>
+      <div className="mbti-description">
+        <p className="mbti-sum">새학기 인싸</p>
+        <div className="mbti-des-box">
+          <p className="des">어쩌구 저쩌구 어쩔 뚱땡이 저쩔 냉장고</p>
+        </div>
+      </div>
+      <div className="button-wrapper">
+        <div className="text">
+          <div>다시하기</div>
+          <div>홈으로</div>
+          <div>공유하기</div>
+        </div>
+        <div className="icons">
+          <div>
+            <MdOutlineReplay className="return" />
+          </div>
+          <div>
+            <AiFillHome className="home" />
+          </div>
+          <div>
+            <HiOutlineLink className="share" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default OwnerResult;
