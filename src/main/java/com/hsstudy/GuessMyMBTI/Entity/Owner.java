87377de994f7e86d25c78cc9763815b64957057a
JongukYang <didwhddnr123@naver.com>
2023-02-09 16:50:17 +0900

    datasource, jpa, 설정

--- src/main/java/com/hsstudy/GuessMyMBTI/Entity/Owner.java
+++ src/main/java/com/hsstudy/GuessMyMBTI/Entity/Owner.java
@@ -9,12 +9,17 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // no기본 생성자
+@AllArgsConstructor // args 객체가 가지고 있는 변수가 가지고 있는 생성자를 민들어줌
 @Builder
 public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "OWNER_ID")
     private Long id;
+    @Column(name = "OWNER_NICKNAME")
+    private String nickname; // 카톡 이름, 구글 이름
+
+
 
 }
