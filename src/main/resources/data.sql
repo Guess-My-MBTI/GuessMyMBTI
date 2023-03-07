6dddf9e7261f9c808b52d654844cebce69a60e98
JongukYang <didwhddnr123@naver.com>
2023-03-07 16:13:28 +0900

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,36 @@
 insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
-values ("ESFJ", "양종욱이랑 비슷한놈", "이친구는 아주 사교적이고 노는걸 좋아하는 뽀로로입니다");
-insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
 values ("ISTJ", "책임감 많은 학급회장", "가나다라마바사");
 insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
 values ("ISFJ", "성실한 학급 총무", "어쩌구 저쩌구");
 insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
 values ("INFJ", "특정 집단 활동에서 리더쉽 있는 학생", "뭐임마?");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("INTJ", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ISTP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ISFP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("INFP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("INTP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ESTP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ESFP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ENFP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ENTP", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ESTJ", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ESFJ", "양종욱이랑 비슷한놈", "이친구는 아주 사교적이고 노는걸 좋아하는 뽀로로입니다");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ENFJ", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
+insert into ExplainMbti (EXPLAINMBTI_MBTI, EXPLAINMBTI_NM, EXPLAINMBTI_CT)
+values ("ENTJ", "Explain MBTI : 더미 데이터", "MBIT CharType : Dummy Data");
 
 insert into Question (QUESTION_ID, QUESTION_TYPE, QUESTION_CT, QUESTION_A1, QUESTION_A2)
 values (1, "EI", "콘텐츠 쫠롸쏼롸", "E", "I");
