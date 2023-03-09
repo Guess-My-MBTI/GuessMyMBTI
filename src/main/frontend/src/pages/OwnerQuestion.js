51a6e7a934b3a10d8e8c1c6ee765587f115c2e97
SU_VIN <pyotnqls25@naver.com>
2023-03-09 19:12:06 +0900

    OwnerQuestion update
    배열 공백들어가는 이슈 해결 완료

--- src/main/frontend/src/pages/OwnerQuestion.js
+++ src/main/frontend/src/pages/OwnerQuestion.js
@@ -1,22 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const OwnerQuestion = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
-  const [id, setId] = useState(1);
+  const [id, setId] = useState(0);
+  const [Qid, setQid] = useState(1);
+  const [answer, setAnswer] = useState([]);
 
   const baseUrl = "http://localhost:8080/";
 
-  const list = data.filter((it) => parseInt(it.id) == parseInt(id));
-  const [answer, setAnswer] = useState([]);
-
-  console.log(answer);
-
   const accessToken = localStorage.getItem("access_token");
-
-  console.log(accessToken);
 
   //question list 불러옴
   useEffect(() => {
@@ -28,44 +23,62 @@ const OwnerQuestion = () => {
         Authorization: "Bearer " + accessToken,
       },
     }).then((res) => {
+      console.log(res.data.data);
       setData(res.data.data);
     });
   }, []);
 
-  const decreaseQuestion = () => {
-    if (id <= 1) {
-      navigate({ response: true });
-    } else {
-      setId(id - 1);
-
-      // 뒤로 가면 선택됐던 값 삭제되도록 (다시 선택할거니까)
-      answer.splice(answer.indexOf(answer.length - 1), 1);
-      setAnswer([...answer]);
+  //answer이 변화할때만 id에 변화를줘야함
+  useEffect(() => {
+    console.log(answer);
+    if (id == 20) {
+      calcMbti(answer);
     }
-  };
-
-  const increaseQuestion = () => {
-    if (id >= 20) {
-      navigate("/owner-result");
-    } else {
+    if (answer.length < 20) {
       setId(id + 1);
     }
+  }, [answer]);
+
+  //번호대로 불러온 질문과 답
+  const list = data.filter((it) => it.id == id);
+
+  //버튼 누를시 작동
+  const answerHandeler = (e) => {
+    if (e.target.className === "answer-card-1") {
+      setAnswer(answer.concat(list?.map((it) => it.answer1).slice(0, 1)));
+    } else {
+      setAnswer(answer.concat(list?.map((it) => it.answer2).slice(0, 1)));
+    }
   };
 
-  const answerHandler = (e) => {
-    // 첫 번째 보기 선택
-    if (e.target.className == "answer-card-1") {
-      setAnswer(answer.concat(list.map((it) => it.answer1).slice(0, 1)));
-    }
-    // 두 번째 보기 선택
-    else {
-      setAnswer(answer.concat(list.map((it) => it.answer2).slice(0, 1)));
-    }
+  const calcMbti = (answer) => {
+    const EI = answer.slice(0, 5);
+    const NS = answer.slice(5, 10);
+    const FT = answer.slice(10, 15);
+    const PJ = answer.slice(15, 20);
+    console.log(EI);
+    console.log(NS);
+    console.log(FT);
+    console.log(PJ);
 
-    // 정답 중복 선택을 방지하기 위해 답을 선택하면 바로 다음 문제로 넘어가도록 함
-    {
-      increaseQuestion();
-    }
+    const countE = EI.filter((e) => "E" === e).length;
+    const countI = EI.filter((e) => "I" === e).length;
+    const countN = NS.filter((e) => "N" === e).length;
+    const countS = NS.filter((e) => "S" === e).length;
+    const countF = FT.filter((e) => "F" === e).length;
+    const countT = FT.filter((e) => "T" === e).length;
+    const countP = PJ.filter((e) => "P" === e).length;
+    const countJ = PJ.filter((e) => "J" === e).length;
+
+    const mbti =
+      (countE > countI ? "E" : "I") +
+      (countN > countS ? "N" : "S") +
+      (countF > countT ? "F" : "T") +
+      (countP > countJ ? "P" : "J");
+
+    console.log(mbti);
+    localStorage.setItem("mbti", mbti);
+    navigate("/owner-result");
   };
 
   return (
@@ -91,14 +104,7 @@ const OwnerQuestion = () => {
           <span className={["proGraph", `proGraph_${id}`].join(" ")}></span>
         </div>
         <div className="progress-btn">
-          <button className="prev" onClick={decreaseQuestion}>
-            {" "}
-            {"<"}PREV{" "}
-          </button>
-          {/* <button className="next" onClick={increaseQuestion}>
-              {" "}
-              NEXT{">"}
-            </button> */}
+          <button className="prev"> {"<"}PREV </button>
         </div>
       </div>
 
@@ -113,22 +119,19 @@ const OwnerQuestion = () => {
         </div>
 
         <div className="answer-card-1">
-          <div
-            className="answer-card-1"
-            value="answer1"
-            onClick={answerHandler}
-          >
-            <p className="ans-text-1">{list.map((it) => it.answer1)}</p>
+          <div className="answer-card-1" onClick={answerHandeler}>
+            <p className="ans-text-1">
+              {/* 전체 데이터 들어오면 slice(1)로 바꿔야함 */}
+              {list.map((it) => it.answer1.slice(0, 1))}
+            </p>
           </div>
         </div>
 
         <div className="answer-card-2">
-          <div
-            className="answer-card-2"
-            value="answer2"
-            onClick={answerHandler}
-          >
-            <p className="ans-text-2">{list.map((it) => it.answer2)}</p>
+          <div className="answer-card-2" onClick={answerHandeler}>
+            <p className="ans-text-2">
+              {list.map((it) => it.answer2.slice(0, 1))}
+            </p>
           </div>
         </div>
       </div>
