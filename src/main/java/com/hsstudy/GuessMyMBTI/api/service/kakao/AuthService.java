eeab8363af1e6311248e0d386af3692c689168d8
JongukYang <didwhddnr123@naver.com>
2023-03-27 17:58:35 +0900

    guest List 예외처리 삭제

--- src/main/java/com/hsstudy/GuessMyMBTI/api/service/kakao/AuthService.java
+++ src/main/java/com/hsstudy/GuessMyMBTI/api/service/kakao/AuthService.java
@@ -284,10 +284,10 @@ public class AuthService {
 
         List<Guest> guests = account.getGuests();
 
-        if (guests.size() > 1) {
-            // 예외 처리 등
-            return ResponseEntity.ok().body("none Guests");
-        }
+//        if (guests.size() > 1) {
+//            // 예외 처리 등
+//            return ResponseEntity.ok().body("none Guests");
+//        }
 
         // guest 완료한사람만 출력하기
         ArrayList<Guest> guestList = new ArrayList<>();
@@ -347,10 +347,9 @@ public class AuthService {
 
         List<Guest> guests = account.getGuests();
 
-        if (guests.size() > 1) {
-            return ResponseEntity.ok().body("none guestes");
-        }
-
+//        if (guests.size() > 1) {
+//            return ResponseEntity.ok().body("none guestes");
+//        }
 
         for (Guest guest : guests) {
             guest.setOwner(null);
