c6600d04f2ceca5bc426bc3469bb8aca9f9f6a68
JongukYang <didwhddnr123@naver.com>
2023-05-28 00:39:14 +0900

    Update WebConfig.java

--- src/main/java/com/hsstudy/GuessMyMBTI/config/WebConfig.java
+++ src/main/java/com/hsstudy/GuessMyMBTI/config/WebConfig.java
@@ -6,19 +6,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    /* 프론트 서버와의 CORS 이슈를 해결하기 위한 메소드입니다.*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/login/oauth2/callback/kakao")
-//                .allowedOrigins("http://localhost:3000/")
-                .allowedOrigins("https://gmmclients.click/")
-                .allowedOriginPatterns("*")
-                .allowedHeaders("*")
-                // 내가 직접 만든 헤더 이름을 받을 수 있도록 명시해줍시다
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://gmmclients.click")
+                .allowedHeaders("Authorization", "Set-Cookie")
                 .exposedHeaders("Authorization", "Set-Cookie")
                 .allowedMethods("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
+    /* 프론트 서버와의 CORS 이슈를 해결하기 위한 메소드입니다.*/
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+////                .allowedOrigins("http://localhost:3000/")
+//                .allowedOrigins("https://gmmclients.click/")
+//                .allowedOriginPatterns("*")
+//                .allowedHeaders("*")
+//                // 내가 직접 만든 헤더 이름을 받을 수 있도록 명시해줍시다
+//                .exposedHeaders("Authorization", "Set-Cookie")
+//                .allowedMethods("*")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowCredentials(true);
+//    }
+
 }
